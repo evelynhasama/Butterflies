8497ac60970f920a25cd93a46b8fd3e616c1f451
Evelyn Hasama <ehasama11@gmail.com>
2020-12-28 12:44:41 -0800

    Butterflies

--- Butterflies/ViewController.swift
+++ Butterflies/ViewController.swift
@@ -9,9 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var qod: UILabel!
+
+    @IBOutlet weak var image1: UIImageView!
+    
+    @IBOutlet weak var image2: UIImageView!
+    
+    @IBOutlet weak var image3: UIImageView!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        
+        image1.layer.cornerRadius = 15
+        image2.layer.cornerRadius = 15
+        image3.layer.cornerRadius = 15
     }
 
 
