69053724d1862ee3924918b098f56482d0e62744
Evelyn Hasama <ehasama11@gmail.com>
2020-12-28 14:47:21 -0800

    More Developments

--- Butterflies/ViewController.swift
+++ Butterflies/ViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var quoteod: String!
     
     @IBOutlet weak var qod: UILabel!
 
@@ -21,14 +22,42 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+       
+        var request = URLRequest(url: URL(string: "https://quotes.rest/qod")!)
+        request.httpMethod = "GET"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+
+        URLSession.shared.dataTask(with: request, completionHandler: { data, response, error -> Void in
+            do {
+                print(data!)
+                print(response!)
+                let jsonDecoder = JSONDecoder()
+                // Access the response here by using json model class
+                // You can autogenerate Json4Swift_Base swift class below by pasting the JSON response in
+                // the webpage http://www.json4swift.com
+                let responseModel = try jsonDecoder.decode(Json4Swift_Base.self, from: data!)
+                print(responseModel)
+                print(responseModel.contents?.quotes?.first!.quote as Any)
+                self.quoteod = responseModel.contents?.quotes?.first!.quote
+                
+                self.Setquote()
+            } catch {
+                print("JSON Serialization error")
+            }
+        }).resume()
         
         
         image1.layer.cornerRadius = 15
         image2.layer.cornerRadius = 15
         image3.layer.cornerRadius = 15
     }
-
+    func Setquote(){
+        DispatchQueue.main.async {
+            self.qod.text = self.quoteod
+        }
+    }
 
 }
+
 
